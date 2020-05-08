a16c1537a2ee5a93b9d1873a751de2796aef80f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-08 14:55:24 +0000

    Deploy whitakerchancellor/hackernews to github.com/whitakerchancellor/hackernews.git:gh-pages

---
+++ precache-manifest.a0d6a174eeedf3bf96751a0456d5a79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c3cf3224f47de3c63f39eec6704f62",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "b7c81a17eed0fb864176",
+    "url": "/hackernews/static/css/main.5af46d8e.chunk.css"
+  },
+  {
+    "revision": "34e5496505f60a766d26",
+    "url": "/hackernews/static/js/2.2d16c575.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/hackernews/static/js/2.2d16c575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c81a17eed0fb864176",
+    "url": "/hackernews/static/js/main.46c1d6b6.chunk.js"
+  },
+  {
+    "revision": "20c6fd22f439b416a2b5",
+    "url": "/hackernews/static/js/runtime-main.daf06b6c.js"
+  }
+]);
